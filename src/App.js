a0d5555f824898cb5a2f5aadbe2a8d6f3033df65
Vishnuraj <vishnuraj.arjunasamy@cdw.com>
2024-09-13 16:53:00 +0530

    Setup folder structure, add users data and basic component structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import UsersArea from "./Containers/UsersArea/UsersArea";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Users</h1>
+      <Navbar />
+      <UsersArea />
     </div>
   );
 }

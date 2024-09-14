ca14fa9c37c5ab9b63646197b53e68dec6d7ac64
Vishnuraj <vishnuraj.arjunasamy@cdw.com>
2024-09-14 17:09:26 +0530

    Fixed users heading

--- src/Pages/Users/Users.js
+++ src/Pages/Users/Users.js
@@ -15,7 +15,7 @@ export default function Users() {
   return (
     <div className="flex flex-wrap justify-center content-center w-screen h-screen bg-lavenderMist">
       <div className="flex flex-col bg-white w-4/5 h-4/5 lg:h-auto lg:p-[100px]  mx-auto rounded-2xl overflow-scroll">
-        {/* <h1 className="font-merri text-3xl font-black text-mirage">Users</h1> */}
+        <h1 className="font-merri text-4xl font-black text-mirage">Users</h1>
         <Navbar setSearch={setSearch} />
         <UsersArea usersData={users} />
       </div>

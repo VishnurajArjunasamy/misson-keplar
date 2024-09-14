842879f3cebffd7eafce6cb0923ea198a93411ed
Vishnuraj <vishnuraj.arjunasamy@cdw.com>
2024-09-14 16:18:24 +0530

    Implement Navbar with styles

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,14 +4,14 @@ import Input from "../Common/Input";
 export default function Navbar() {
   const menus = ["Reputation", "New users", "Voters", "Editors", "Moderators"];
   const menusList = menus.map((menu, idx) => {
-    return <li key={idx} className="font-poppins">{menu}</li>;
+    return <li key={idx} className="w-[150px] px-4 py-5 text-center my-auto hover:text-white hover:bg-periwinkleBlue rounded-lg">{menu}</li>;
   });
   return (
-    <nav className="flex w-full justify-betwee">
-      <div className="w-2/6 h-[60px]">
+    <nav className="flex w-full h-24 justify-between mt-10 mb-16">
+      <div className="w-[33.33%] h-full flex-initial">
         <Input className='w-full h-full pl-14 pr-3 py-4 text-mirage border border-lavenderMist rounded-md focus:outline-none focus:border-periwinkleBlue focus:ring-periwinkleBlue placeholder:opacity-1 bg-searchIcon bg-no-repeat bg-[center_left_1rem] bg-[length:32px_32px]' placeholder={'Search users'} type={'search'}/>
       </div>
-      {/* <ul className="flex justify-between w-3/5 p-4">{menusList}</ul> */}
+      <ul className="flex justify-between items-center w-[600px] flex-initial font-poppins text-mirage">{menusList}</ul>
     </nav>
   );
 }

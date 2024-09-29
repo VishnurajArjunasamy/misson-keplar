ace53e1fc27b62fa5e375501ca397d62f07ba1c3
Vishnuraj <vishnuraj.arjunasamy@cdw.com>
2024-09-29 15:36:08 +0530

    restaurant-card styling

---
+++ src/components/food-type-icon/food-type.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./food-type.module.scss";
+
+interface TypeProp {
+  type: string;
+}
+export default function FoodType({ type }: TypeProp) {
+  return (
+    <div
+      className={type == "Veg" ? styles["veg-logo"] : styles["non-veg-logo"]}
+    >
+      <div></div>
+    </div>
+  );
+}
--- src/components/restaurant-card/restaurant-card.tsx
+++ src/components/restaurant-card/restaurant-card.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import styles from "./restaurant-card.module.scss";
 import { RestaurantIF } from "../../types/restaurantType";
-import Rating from "../rating/rating";
 import { Link } from "react-router-dom";
+import Rating from "../rating/rating";
+import certfiedImg from "../../assets/images/certify.png";
+import vegIcon from "../../assets/images/veg-icon.png";
+import nonVegIcon from "../../assets/images/non-veg-icon.png";
+import styles from "./restaurant-card.module.scss";
+import FoodType from "../food-type-icon/food-type";
 
 interface PropsIF {
   data?: RestaurantIF;
@@ -13,16 +17,32 @@ export default function RestaurantCard({ data }: PropsIF) {
     <div className={styles["res-card"]}>
       <img src={data?.restaurantImageSrc} />
       <div className={styles["info"]}>
-        <h1>{data?.name}</h1>
-        <div>
+        <div className={styles["info-header"]}>
+          <h1>{data?.name}</h1>
+          <div className={styles["logos"]}>
+            <i>
+              <img src={certfiedImg} />
+            </i>
+            <>
+              {data?.type.map((type) => (
+                <i>
+                  <img src={type == "Veg" ? vegIcon : nonVegIcon} />
+                </i>
+              ))}
+            </>
+          </div>
+        </div>
+        <div className={styles["info-content"]}>
           <div>
             <h2>{data?.location}</h2>
             <h3>{data?.timings}</h3>
           </div>
           <Rating />
         </div>
-        <div>{data?.cuisine.join(",")}</div>
-        <Link to={`/restaurants/${data?.id}`} state={data}>VIEW MORE DETAILS</Link>
+        <div className={styles["cuisine-txt"]}>{data?.cuisine.join(",")}</div>
+        <Link to={`/restaurants/${data?.id}`} className={styles["link-txt"]}>
+          <p>VIEW MORE DETAILS</p> <i>{">"}</i>
+        </Link>
       </div>
     </div>
   );

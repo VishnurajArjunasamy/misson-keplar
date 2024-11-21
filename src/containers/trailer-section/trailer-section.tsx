a993aeb42b1ff3ca49f072c4174bbcc6dacb88c7
Vishnuraj <vishnuraj.arjunasamy@cdw.com>
2024-11-21 12:42:38 +0530

    trailers section completed

--- src/containers/trailer-section/trailer-section.tsx
+++ src/containers/trailer-section/trailer-section.tsx
@@ -1,18 +1,27 @@
 import styles from "./trailer-section.module.scss";
-import { Link } from "react-router-dom";
-import { SESSION, TRAILERS } from "../../constants/app-constants";
+import { Link, useNavigate } from "react-router-dom";
+import { MENUS, SESSION, TRAILERS } from "../../constants/app-constants";
 import Button from "../../components/button/button";
 import { memo } from "react";
+import { useAuth } from "../../context/auth-context";
 
 const TrailerSection = () => {
-  const handleClick = () => {};
+  const navigate = useNavigate();
+  const { user } = useAuth();
+  const handleClick = () => {
+    {
+      navigate(`/${MENUS.NOWSHOWING.ROUTE}`);
+    }
+  };
   return (
     <section className={styles["trailer-section"]}>
       <h1>{TRAILERS.HEADING}</h1>
-      <p className={styles["sign-in-prmt"]}>
-        {TRAILERS.SIGN_IN_PROMPT}{" "}
-        <Link to={`/${SESSION.LOGIN.ROUTE}`}>{TRAILERS.SIGN_IN_NOW}</Link>
-      </p>
+      {!user && (
+        <p className={styles["sign-in-prmt"]}>
+          {TRAILERS.SIGN_IN_PROMPT}{" "}
+          <Link to={`/${SESSION.LOGIN.ROUTE}`}>{TRAILERS.SIGN_IN_NOW}</Link>
+        </p>
+      )}
       <div className={styles["split-container"]}>
         <div className={styles["trailer-img"]}></div>
         <div className={styles["trailer-des"]}>

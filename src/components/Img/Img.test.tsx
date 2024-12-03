25a8e5063cccdb60d0e7161774e92fad3d59c9f2
Vishnuraj <vishnuraj.arjunasamy@cdw.com>
2024-12-03 22:07:33 +0530

    Image test coverage

--- src/components/Img/Img.test.tsx
+++ src/components/Img/Img.test.tsx
@@ -1,11 +1,25 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Img from "./Img";
 
-it("Image should be rendered with the src", () => {
-  const imgSrc = "../../assets/images/sindel-background.png";
-  render(<Img src={imgSrc} />);
-  const imgElement = screen.getByRole("img");
-  expect(imgElement).toBeInTheDocument();
-  expect(imgElement).toHaveAttribute("src", imgSrc);
+const imgSrc = "../../assets/images/sindel-background.png";
+
+jest.mock("../../assets/empty-image.png", () => "empty-image-mock.png");
+
+
+describe("Img Component", () => {
+  test("renders correctly with a valid src", () => {
+    render(<Img src={imgSrc} />);
+    const img = screen.getByRole("img");
+    expect(img).toHaveAttribute("src", imgSrc);
+    expect(img).toHaveAttribute("alt", "Movie");
+    expect(img).toHaveClass("img-style");
+  });
+
+  test("replaces src with emptyImage when an error occurs", () => {
+    render(<Img src="broken-image.png" />);
+    const img = screen.getByRole("img");
+    fireEvent.error(img);
+    expect(img).toHaveAttribute("src", "empty-image-mock.png");
+  });
 });

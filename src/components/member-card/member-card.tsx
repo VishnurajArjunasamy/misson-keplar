d0914dd73d907f0cef0e4e3f4dc782c5a16ffd42
Vishnuraj <vishnuraj.arjunasamy@cdw.com>
2025-01-02 19:56:32 +0530

    members container completed

--- src/components/member-card/member-card.tsx
+++ src/components/member-card/member-card.tsx
@@ -1,5 +1,8 @@
 import { FC } from "react";
 import Img from "../Img/img";
+import classes from "./member-card.module.scss";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
 
 interface MemberCardProps {
   image: string;
@@ -8,8 +11,15 @@ interface MemberCardProps {
 }
 
 const MemberCard: FC<MemberCardProps> = ({ image, name, city }) => {
+  const isDarkMode = useSelector(
+    (state: RootState) => state.sideBar.isDarkMode
+  );
   return (
-    <div>
+    <div
+      className={`${classes.member} ${
+        isDarkMode ? classes.dark : classes.light
+      }`}
+    >
       <Img src={image} alt={name} />
       <h1>{name}</h1>
       <h2>{city}</h2>
